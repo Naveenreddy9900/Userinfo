416fcad8f7a415293ec5c7aeccc7383bb60dd372
Naveenreddy9900 <naveen@clistas.com>
2024-08-22 10:20:44 +0530

    first commit

---
+++ src/main/java/com/example/jwt/repository/UserInfoRepository.java
@@ -0,0 +1,11 @@
+package com.example.jwt.repository;
+import com.example.jwt.entity.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+    Optional<UserInfo> findByUsername(String username);
+}
